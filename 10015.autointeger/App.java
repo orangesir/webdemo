e0ad6f221aa469c0fbf21833f6273047df3a7a85
zhangweid <zhangweid@zbj.com>
2018-01-03 10:54:08 +0800

    面试题和自动取整

---
+++ 10015.autointeger/App.java
@@ -0,0 +1,11 @@
+
+class App {
+
+  public static void main(String[] args) {
+      float i = 11/2;
+      System.out.println(i);
+      double b = 11.0/2;
+      System.out.println(b);
+  }
+
+}
---
+++ 10015.autointeger/AppW.java
@@ -0,0 +1,8 @@
+class AppW{
+
+  public static void main(String[] args) {
+    float i=11.0/2;
+    System.out.println(i);
+  }
+
+}
